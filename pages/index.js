29f6fcfce5b5cf1cbf957b76d32bf1725a36f84a
dablitz234 <113091823+dablitz234@users.noreply.github.com>
2022-12-14 11:51:04 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+
+import axios from "axios";
+
+export default function Auth() {
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    if (username.length === 0 || secret.length === 0) return;
+
+    axios
+      .put(
+        "https://api.chatengine.io/users/",
+        { username, secret },
+        { headers: { "Private-key": "6d2f0f87-8380-46ec-9a77-1f81f2204ed1" } }
+      )
+      .then((r) => router.push("/chats"));
+  }
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => onSubmit()}>
+          <div className="authtitle">Chatify, By David Blitz</div>
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Login / Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
